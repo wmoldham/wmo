68be3fe649411dc5d4c1054b3eb785c3fb641c37
William Oldham <wm.oldham@gmail.com>
2021-03-15 14:35:09 -0400

    document %nin% function

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,10 @@
+#' Negate %in%
+#'
+#' Binary operator that returnrs a logical vector returning `TRUE` if there is
+#' not a match for its left operand.
+#'
+#' @param x The values to be matched
+#' @param y The values to be matched against
+#'
 #' @export
 "%nin%" <- function(x, y) {!(x %in% y)}
