3325a1568b599a125fc134df0fab713755dab8d8
William Oldham <wm.oldham@gmail.com>
2021-03-16 09:08:32 -0400

    add read_multi_excel function

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 #' Negate %in%
 #'
-#' Binary operator that returnrs a logical vector returning `TRUE` if there is
+#' Binary operator that returns a logical vector returning `TRUE` if there is
 #' not a match for its left operand.
 #'
 #' @param x The values to be matched
@@ -8,3 +8,28 @@
 #'
 #' @export
 "%nin%" <- function(x, y) {!(x %in% y)}
+
+
+#' Reads and combines multi-sheet Excel files
+#'
+#' This function extracts and combines Excel sheets from multiple files.
+#'
+#' @param excel_file_list A preferably named list of Excel files.
+#'
+#' @return A list of tibbles for each unique Excel sheet in the submitted files.
+#' @export
+#'
+read_multi_excel <- function(excel_file_list) {
+  sheets <- unique(unlist(purrr::map(excel_file_list, readxl::excel_sheets)))
+  purrr::map(
+    sheets,
+    ~purrr::map_dfr(
+      excel_file_list,
+      readxl::read_excel,
+      sheet = .x,
+      .id = "experiment",
+      col_types = "text"
+    )
+  ) %>%
+    rlang::set_names(sheets)
+}
